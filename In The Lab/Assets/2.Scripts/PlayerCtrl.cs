43f25176130940adf1e4fec9166c9c40bfda3ff3
koojina <70617718+koojina@users.noreply.github.com>
2020-11-11 20:24:13 +0900

    3차(이동)

--- In The Lab/Assets/2.Scripts/PlayerCtrl.cs
+++ In The Lab/Assets/2.Scripts/PlayerCtrl.cs
@@ -8,17 +8,18 @@ public class PlayerCtrl : MonoBehaviour
     private float h = 0.0f;
     private float v = 0.0f;
     private Transform tr;
-    public float moveSpeed = 10.0f; //이동 속도 변수
+    public float moveSpeed = 7f; //이동 속도 변수
     
     CharacterController CC; //캐릭터 컨트롤러 변수
     float gravity = -20f; //중력 변수
     float yVelocity = 0; // 수직 속력 변수
     public float jump = 10f;
+    public bool isJumping = false; //점프 상태 변수
 
-     public void start()
+     public void Start()
     {
         tr = GetComponent<Transform>();
-      //  CC = GetComponent<CharacterController>();//시작과 동시에 캐릭터 컨트롤러 컴포넌트 할당 받기 위해 
+       CC = GetComponent<CharacterController>();//시작과 동시에 캐릭터 컨트롤러 컴포넌트 할당 받기 위해 
     }
     
     // Start is called before the first frame update
@@ -31,17 +32,23 @@ public class PlayerCtrl : MonoBehaviour
         v = Input.GetAxis("Vertical");
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         
-        //moveDir = Camera.main.transform.TransformDirection(moveDir); 
+        moveDir = Camera.main.transform.TransformDirection(moveDir);  //메인 카메라 기준으로 방향 변환
         tr.Translate(moveDir * moveSpeed * Time.deltaTime, Space.Self);
-
-       //메인 카메라 기준으로 방향 변환
        
-        /*if (Input.GetButtonDown("Jump"))
+       
+        
+        if(isJumping && CC.collisionFlags == CollisionFlags.Below)//만약 점프 중 & 다시 바닥(Below) 착지
         {
-            yVelocity = jump; //수직 속력에 점프력 적용
+            isJumping = false; //점프 전 상태로 초기화
+            yVelocity = 0;
+        }
+        if (Input.GetButtonDown("Jump") && !isJumping)//만약 점프키를 누름 & 점프 X
+        {
+            yVelocity = jump;
+            isJumping = true; //점프 상태로 변경
         }
         yVelocity += gravity * Time.deltaTime;
         moveDir.y = yVelocity; //캐릭터 수직 속도에 중력 적용
-        CC.Move(moveDir * moveSpeed * Time.deltaTime); //이동*/
+        CC.Move(moveDir * moveSpeed * Time.deltaTime); //이동
     }
 }
